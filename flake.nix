8f6d516e72a0cfe1d75cb00c745d9dc701919bc6
rydnr <github@acm-sl.org>
2024-10-28 18:55:30 +0100

    Updated nixpkgs

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
   description = "Application layer of pythoneda-artifact/nix-flake";
   inputs = rec {
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-    nixos.url = "github:NixOS/nixpkgs/23.11";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
     pythoneda-artifact-nix-flake = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
